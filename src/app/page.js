932f59ae17f8ea6a3fa5ac41033c7c87e9743923
Rakib-Akanda <rmd04037@gmail.com>
2025-04-26 23:07:54 +0600

    fix: daisyui problem

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,8 @@ export default function Home() {
   // console.log(name);
   return (
     <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
-      hello next hero 
+      <p className="text-5xl">hello next hero </p>
+      <button className="btn btn-xl">Xlarge</button>
       <Heading></Heading>
       <button className="text-5xl">Submit </button>
     </div>
