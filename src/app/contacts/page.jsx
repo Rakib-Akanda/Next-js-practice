1069fc588c20b23df2d9588ce80c31d43a0cf250
Rakib-Akanda <rmd04037@gmail.com>
2025-04-26 23:21:57 +0600

    hero added in contacts page

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,8 +1,57 @@
 const Contacts = () => {
   // console.log(hello);
   return (
-    <div>
-      <h2>This Contact Page</h2>
+    <div className="m-10 h-screen">
+      <div className="hero bg-base-200 min-h-screen border-2">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+            {/* drawer */}
+            <div className="drawer drawer-end my-4">
+              <input
+                id="my-drawer-4"
+                type="checkbox"
+                className="drawer-toggle"
+              />
+              <div className="drawer-content">
+                {/* Page content here */}
+                <label
+                  htmlFor="my-drawer-4"
+                  className="drawer-button btn btn-primary"
+                >
+                  Open drawer
+                </label>
+              </div>
+              <div className="drawer-side">
+                <label
+                  htmlFor="my-drawer-4"
+                  aria-label="close sidebar"
+                  className="drawer-overlay"
+                ></label>
+                <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                  {/* Sidebar content here */}
+                  <li>
+                    <a>Sidebar Item 1</a>
+                  </li>
+                  <li>
+                    <a>Sidebar Item 2</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
