033bc484834f74a72c31608052a1e8146dc35760
Rakib-Akanda <rmd04037@gmail.com>
2025-05-02 02:30:06 +0600

    update: support both dev and prod cookie names in middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -8,11 +8,30 @@ export const middleware = async (request) => {
   // if (!cookies || cookies.value !== co) {
   //   return NextResponse.redirect(new URL("/api/auth/signin", request.url));
   // }
-  const cookieStore = await cookies(request);
-  const token = cookieStore.get("next-auth.session-token");
-  console.log(token);
+
+  // const token = request.cookies.get("__Secure-next-auth.session-token");
+  const token =
+    request.cookies.get("__Secure-next-auth.session-token") ||
+    request.cookies.get("next-auth.session-token");
+  // karon deploy er chagne hoie jai
+  /* হ্যাঁ, ✅ NextAuth.js (Next.js এর জন্য authentication library) নিজেই cookies-এর নাম production environment-এ automatic পরিবর্তন করে:
+
+🔄 Cookie Name পরিবর্তনের ব্যপারটি:
+Environment	Cookie Name
+Development	next-auth.session-token
+Production (HTTPS)	__Secure-next-auth.session-token
+🔒 কেন এমন করে?
+এইটা security জন্য করা হয়:
+__Secure- prefix শুধু তখনি কাজ করে যখন:
+Cookie Secure flag সহ সেট করা হয় (i.e., HTTPS connection only)
+এটি browser-এ trustable হিসেবে ধরা হয়
+এটা ensures করে যে:
+Cookie গুলো HTTPS ছাড়া পাঠানো যাবে না
+এবং cross-site scripting (XSS) attack থেকেও নিরাপদ থাকে কিছুটা
+    */
+  console.log("cookies", token);
   if (!token) {
-    return NextResponse.redirect(new URL("/api/auth/signin", request.url)); //(request.url) die amra je page theke aschi shei page redirect kore dei, ete amader private route kora lage nah helpful onk next.js automatic kore dei private route 
+    return NextResponse.redirect(new URL("/api/auth/signin", request.url)); //(request.url) die amra je page theke aschi shei page redirect kore dei, ete amader private route kora lage nah helpful onk next.js automatic kore dei private route
   }
 
   return NextResponse.next();
